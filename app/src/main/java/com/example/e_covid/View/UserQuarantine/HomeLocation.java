e51653357c77dbbe76dde58508bbe2368ab85382
robot <robot>
2022-06-02 17:17:59 +0300

    2 update

---
+++ app/src/main/java/com/example/e_covid/View/UserQuarantine/HomeLocation.java
@@ -0,0 +1,40 @@
+package com.example.e_covid.View.UserQuarantine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+//import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.e_covid.R;
+
+public class HomeLocation extends AppCompatActivity {
+
+    public ImageView Image1;
+    public ImageView Image2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_location);
+
+        Image1 = findViewById (R.id.backtoHdata);
+        Image1.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeLocation.this,HomeQuarantineData.class);
+                startActivity(intent);
+            }
+        });
+        Image2 = findViewById (R.id.Home1);
+        Image2.setOnClickListener (new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeLocation.this, UserRegistration.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
