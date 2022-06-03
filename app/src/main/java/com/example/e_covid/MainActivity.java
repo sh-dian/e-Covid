bab3ee8a37096d021ebeb577f5b333d837e691b4
dian <sh.alqudree@gmail.com>
2022-06-03 19:29:29 +0800

    Menu Page

--- app/src/main/java/com/example/e_covid/MainActivity.java
+++ app/src/main/java/com/example/e_covid/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.e_covid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Objects;
@@ -18,25 +19,16 @@ public class MainActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
 
-        //Go to Admin Login Page
         mAdmin = findViewById(R.id.adminPage);
-        mAdmin.setOnClickListener(view -> {
-            //Intent intent = new Intent(MainActivity.this, Admin_QuarantineCenterMenu.class);
-            //startActivity(intent);
-        });
-
-        //Go to MOH staff Login Page
-        mMoh = findViewById(R.id.MOHPage);
-        mMoh.setOnClickListener(view -> {
-            //Intent intent = new Intent(MainActivity.this, Admin_QuarantineCenterMenu.class);
-            //startActivity(intent);
-        });
-
-        //Go to User Login Page
         mUser = findViewById(R.id.userPage);
-        mUser.setOnClickListener(view -> {
-            Intent intent = new Intent(MainActivity.this, User_MenuPage.class);
-            startActivity(intent);
+        mMoh = findViewById(R.id.MOHPage);
+
+        mUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, User_MenuPage.class);
+                startActivity(intent);
+            }
         });
     }
 }
