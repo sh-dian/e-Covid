0f5c91758fee40879670f7bf5be36eecdcad497a
dian <sh.alqudree@gmail.com>
2022-06-03 19:32:07 +0800

    Integrate Page

--- app/src/main/java/com/example/e_covid/User_MenuPage.java
+++ app/src/main/java/com/example/e_covid/User_MenuPage.java
@@ -3,6 +3,7 @@ package com.example.e_covid;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.e_covid.View.QuarantineCenter.Admin_QuarantineCenterMenu;
@@ -21,9 +22,12 @@ public class User_MenuPage extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         qc = findViewById(R.id.quarantineCenter_button);
-        qc.setOnClickListener(view -> {
-            Intent intent = new Intent(User_MenuPage.this, Admin_QuarantineCenterMenu.class);
-            startActivity(intent);
+        qc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(User_MenuPage.this, Admin_QuarantineCenterMenu.class);
+                startActivity(intent);
+            }
         });
     }
 }
