7a04cb1f6bf6667a3ba24e8f2bfd80e2f52169c2
amymazlan <amymohdmazlan999@gmail.com>
2022-06-03 19:35:22 +0800

    copy paste xml n java file

---
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/checkin_history.java
@@ -0,0 +1,17 @@
+package com.example.e_covid.View.CheckIn_CheckOut;
+
+import android.os.Bundle;
+
+//import androidx.appcompat.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.e_covid.R;
+
+public class checkin_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkin_history);
+    }
+}
