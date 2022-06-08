decf7960a38168a9eeea99c9ca3e2bc21fb510bc
amymazlan <amymohdmazlan999@gmail.com>
2022-06-09 01:04:32 +0800

    Check In Check Out xml file

--- app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/user/CheckInMain.java
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/user/CheckInMain.java
@@ -1,6 +1,27 @@
 package com.example.e_covid.View.CheckIn_CheckOut.user;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.View;
+
+import com.example.e_covid.R;
+import com.example.e_covid.View.Login.AdminLogin;
+import com.example.e_covid.View.Login.Admin_MenuPage;
+
+import java.util.Objects;
 
 public class CheckInMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //author code - hide action bar
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_main);
+
+
+    }
 }
