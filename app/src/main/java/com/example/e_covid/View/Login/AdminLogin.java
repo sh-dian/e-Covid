c7d747c027109c8ba76f4b0a8cbbf9b85ce07222
ahmdy <ahmd.yssrbi@gmail.com>
2022-06-08 10:26:53 +0800

    yass: import all java class and xml file

--- app/src/main/java/com/example/e_covid/View/Login/AdminLogin.java
+++ app/src/main/java/com/example/e_covid/View/Login/AdminLogin.java
@@ -12,7 +12,7 @@ public class AdminLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Objects.requireNonNull(getSupportActionBar()).hide();
+        //Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_admin_login);
     }
 }
