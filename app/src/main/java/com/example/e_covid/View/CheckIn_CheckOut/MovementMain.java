1031a226e4850f4c78784541f1146bfcc64f988e
dian <sh.alqudree@gmail.com>
2022-06-09 01:30:56 +0800

    Integration and Movement file

--- app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/MovementMain.java
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/MovementMain.java
@@ -1,18 +1,13 @@
-package com.example.e_covid.View.CheckIn_CheckOut.user;
+package com.example.e_covid.View.CheckIn_CheckOut;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.View;
 
 import com.example.e_covid.R;
-import com.example.e_covid.View.Login.AdminLogin;
-import com.example.e_covid.View.Login.Admin_MenuPage;
 
 import java.util.Objects;
 
-public class CheckInMain extends AppCompatActivity {
+public class MovementMain extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +15,7 @@ public class CheckInMain extends AppCompatActivity {
 
         //author code - hide action bar
         Objects.requireNonNull(getSupportActionBar()).hide();
-        setContentView(R.layout.activity_main);
 
-
+        setContentView(R.layout.activity_movement_main);
     }
 }
