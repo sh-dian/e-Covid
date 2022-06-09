d12222b7cac743258e5e656b1bc9448298bdc9ef
amymazlan <amymohdmazlan999@gmail.com>
2022-06-10 06:14:23 +0800

    admin Check In Check Out  xml file

---
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/admin_search_person.java
@@ -0,0 +1,15 @@
+package com.example.e_covid.View.CheckIn_CheckOut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.e_covid.R;
+
+public class admin_search_person extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_search_person);
+    }
+}
