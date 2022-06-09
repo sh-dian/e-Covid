03dc30152303e769c118d5515b6143f08c6baa02
amymazlan <amymohdmazlan999@gmail.com>
2022-06-10 06:47:50 +0800

    admin Check In Check Out  java file

--- app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/admin_search_person.java
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/admin_search_person.java
@@ -2,14 +2,41 @@ package com.example.e_covid.View.CheckIn_CheckOut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.e_covid.R;
 
 public class admin_search_person extends AppCompatActivity {
 
+    TextView checkinName, checkinICNumber, checkinPhoneNumber;
+    String name, ICNumber, PhoneNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_search_person);
+
+        checkinName = findViewById(R.id.checkinName);
+        checkinICNumber = findViewById(R.id.checkinICnumber);
+        checkinPhoneNumber = findViewById(R.id.checkinPhoneNumber);
+
+        getAndSetIntentData();
+    }
+
+    void getAndSetIntentData(){
+        if(getIntent().hasExtra("name") && getIntent().hasExtra("ICNumber") && getIntent().hasExtra("PhoneNumber")){
+            name = getIntent().getStringExtra("name");
+            ICNumber = getIntent().getStringExtra("ICNumber");
+            PhoneNumber = getIntent().getStringExtra("PhoneNumber");
+
+            //setting intent data
+            checkinName.setText(name);
+            checkinICNumber.setText(ICNumber);
+            checkinPhoneNumber.setText(PhoneNumber);
+
+        }else{
+            Toast.makeText(this, "No Data", Toast.LENGTH_SHORT).show();
+        }
     }
 }
