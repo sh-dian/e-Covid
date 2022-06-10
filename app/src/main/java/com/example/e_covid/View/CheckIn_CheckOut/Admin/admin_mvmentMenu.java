a46e0f02176afc065bc026e5177426d3828130e8
amymazlan <amymohdmazlan999@gmail.com>
2022-06-11 03:16:27 +0800

    movement

---
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/Admin/admin_mvmentMenu.java
@@ -0,0 +1,13 @@
+package com.example.e_covid.View.CheckIn_CheckOut.Admin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class admin_mvmentMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_mvment_menu);
+    }
+}
--- app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/User/CheckIn_Info.java
+++ app/src/main/java/com/example/e_covid/View/CheckIn_CheckOut/User/CheckIn_Info.java
@@ -10,6 +10,8 @@ public class CheckIn_Info extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_check_in_info);
     }
 }
