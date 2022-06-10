47ae534a7faed0e318d77451496e35a75d191514
amymazlan <amymohdmazlan999@gmail.com>
2022-06-11 05:35:07 +0800

    checkin checkout updated

--- app/src/main/java/com/example/e_covid/Controller/CheckIn_CheckOut/CheckIn_CheckOutController.java
+++ app/src/main/java/com/example/e_covid/Controller/CheckIn_CheckOut/CheckIn_CheckOutController.java
@@ -1,4 +1,22 @@
 package com.example.e_covid.Controller.CheckIn_CheckOut;
 
-public class CheckIn_CheckOutController {
-}
+import android.app.Activity;
+import android.database.Cursor;
+
+import com.example.e_covid.Model.CheckIn_CheckOut.CheckIn_CheckOutModel;
+    public class CheckIn_CheckOutController {
+
+        private final CheckIn_CheckOutModel model;
+
+        public CheckIn_CheckOutController(Activity ui) {
+            this.model = new CheckIn_CheckOutModel(ui);
+        }
+
+        public Boolean Add_CIO(String cioName, String cioLocation, String cioPhoneNum, String cioTime, String cioRiskStatus, String cioVaccineStatus) {
+            return model.insertCIO_data(cioName, cioLocation, cioPhoneNum, cioTime, cioRiskStatus, cioVaccineStatus);
+        }
+
+        public Cursor getData() {
+            return model.readAllData();
+        }
+    }
--- app/src/main/java/com/example/e_covid/Model/CheckIn_CheckOut/CheckIn_CheckOutModel.java
+++ app/src/main/java/com/example/e_covid/Model/CheckIn_CheckOut/CheckIn_CheckOutModel.java
@@ -1,4 +1,53 @@
 package com.example.e_covid.Model.CheckIn_CheckOut;
 
-public class CheckIn_CheckOutModel {
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CheckIn_CheckOutModel extends SQLiteOpenHelper {
+
+    public CheckIn_CheckOutModel(Context context) {super(context, "e-Covid.db", null, 1);}
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("CREATE Table CheckIn_CheckOut(cioID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cioName TEXT, cioLocation TEXT, cioPhoneNum TEXT, cioTime TEXT, cioRiskStatus TEXT, cioVaccineStatus TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("DROP Table if exists CheckIn_CheckOut");
+    }
+
+    //insert function
+    public Boolean insertCIO_data(String cioName, String cioLocation, String cioPhoneNum, String cioTime, String cioRiskStatus, String cioVaccineStatus){
+
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put("cioName", cioName);
+        contentValues.put("cioLocation", cioLocation);
+        contentValues.put("cioPhoneNum", cioPhoneNum);
+        contentValues.put("cioTime", cioTime);
+        contentValues.put("cioRiskStatus", cioRiskStatus);
+        contentValues.put("cioVaccineStatus", cioVaccineStatus);
+
+
+        long result = DB.insert("CheckIn_CheckOut", null, contentValues);
+
+        return result != 1;
+    }
+
+    public Cursor readAllData(){
+        String query = "SELECT * FROM CheckIn_CheckOut";
+        SQLiteDatabase DB = this.getReadableDatabase();
+
+        Cursor cursor = null;
+        if(DB != null){
+            cursor = DB.rawQuery(query, null);
+        }
+        return cursor;
+    }
 }
