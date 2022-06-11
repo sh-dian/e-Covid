606737691fbde68ededef83c1c9bdd5e6a4ec200
ahmdy <ahmd.yssrbi@gmail.com>
2022-06-11 23:30:16 +0800

    vaccination certificate form

---
+++ app/src/main/java/com/example/e_covid/UI_Layer/View/Vaccination/ViewVaccinationCertificate.java
@@ -0,0 +1,14 @@
+package com.example.e_covid.UI_Layer.View.Vaccination;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.e_covid.R;
+
+public class ViewVaccinationCertificate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_vaccination_certificate);
+    }
+}
