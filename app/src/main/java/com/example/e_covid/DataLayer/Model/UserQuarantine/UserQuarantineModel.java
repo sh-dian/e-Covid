830bb09b178e54b2f7281b17772d773abd3af3f6
robot <robot>
2022-06-11 19:41:26 +0300

    ,aaaaaaaa

--- app/src/main/java/com/example/e_covid/DataLayer/Model/UserQuarantine/UserQuarantineModel.java
+++ app/src/main/java/com/example/e_covid/DataLayer/Model/UserQuarantine/UserQuarantineModel.java
@@ -1,4 +1,87 @@
 package com.example.e_covid.DataLayer.Model.UserQuarantine;
 
-public class UserQuarantineModel {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.example.e_covid.UI_Layer.View.UserQuarantine.User.HomeQuarantineData;
+
+import java.sql.Date;
+
+public class UserQuarantineModel extends SQLiteOpenHelper {
+
+
+    private Context context;
+    private static final String DATABADE_NAME = "Qregistration.db";
+    private static final int DATABADE_VERGION = 1;
+
+    private static final String TABLE_NAME = "UserQuarantineRegistration";
+    private static final String COLUMN_ID =  "_id";
+    private static final String COLUMN_Type = "QuarantineType";
+    private static final String COLUMN_Result = "TestResult";
+    private static final String COLUMN_DateTest = "DateTest";
+    private static final String COLUMN_CurState = "CurState";
+    private static final String COLUMN_Disable = "Disable";
+    private static final String COLUMN_Chest = "ChestDiseases";
+
+
+
+
+    public UserQuarantineModel(@Nullable Context context) {
+        super(context, DATABADE_NAME, null, DATABADE_VERGION);
+        this.context = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                            COLUMN_Type + " TEXT, " +
+                            COLUMN_Result + " TEXT, " +
+                            COLUMN_DateTest + " INTEGER, " +
+                            COLUMN_CurState + " TEXT, " +
+                            COLUMN_Disable + " BOOLONE, " +
+                            COLUMN_Chest + " BOOLONE);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+
+    }
+
+
+
+
+    public void  addHome(String Result, Integer DateTest , String CurState, Boolean Disable , Boolean Chest) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv =  new ContentValues();
+       // boolean testvalu ;
+        //cv.put(COLUMN_Type, Type);
+        cv.put(COLUMN_Result, Result);
+        cv.put(COLUMN_DateTest, DateTest);
+        cv.put(COLUMN_CurState, CurState);
+        cv.put(COLUMN_Disable, Disable);
+        cv.put(COLUMN_Chest, Chest);
+        long result = db.insert(TABLE_NAME,null, cv);
+        if(result == -1){
+            Toast.makeText(context, "Failed", Toast.LENGTH_SHORT).show();
+           // testvalu = false;
+            //return testvalu;
+        }else{
+            Toast.makeText(context, "AddednSuccessfuly!", Toast.LENGTH_SHORT).show();
+            //testvalu = true;
+            //return testvalu;
+        }
+
+
+    }
 }
--- app/src/main/java/com/example/e_covid/UI_Layer/View/UserQuarantine/User/HomeQuarantineData.java
+++ app/src/main/java/com/example/e_covid/UI_Layer/View/UserQuarantine/User/HomeQuarantineData.java
@@ -5,28 +5,55 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
 
 //import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.e_covid.DataLayer.Model.UserQuarantine.UserQuarantineModel;
 import com.example.e_covid.R;
+
+import java.sql.Date;
 
 public class HomeQuarantineData extends AppCompatActivity {
 
-    public Button button;
+
 
     public ImageView Image1;
     public ImageView Image2;
+
+    EditText QType,  TestResult , DateofTest, CurrentlyState  ;
+    CheckBox Disable, ChestDiseases;
+    boolean testvalu;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_quarantine_data);
 
+
+        TestResult   = findViewById(R.id.TestResult);
+        DateofTest   = findViewById(R.id.DateofTest);
+        CurrentlyState  = findViewById(R.id.CurrentlyState);
+        Disable = findViewById(R.id.Disable);
+        ChestDiseases = findViewById(R.id.ChestDiseases);
+
         button = (Button) findViewById (R.id.Save1);
         button.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                UserQuarantineModel quM = new UserQuarantineModel(HomeQuarantineData.this);
+                quM.addHome(TestResult.getText().toString().trim(),
+                        Integer.valueOf(DateofTest.getText().toString().trim()),
+                        CurrentlyState.getText().toString().trim(),
+                        Boolean.valueOf(Disable.getText().toString().trim()) ,
+                        Boolean.valueOf(ChestDiseases.getText().toString().trim()));
+             
+
                 Intent intent = new Intent(HomeQuarantineData.this, HomeLocation.class);
                 startActivity(intent);
             }
@@ -49,4 +76,5 @@ public class HomeQuarantineData extends AppCompatActivity {
             }
         });
     }
+
 }
